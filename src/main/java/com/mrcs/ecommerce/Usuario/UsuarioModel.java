e72b900424c82fef29a446095b65b3160d422319
Marcos574 <mvdeus574@gmail.com>
2026-03-19 15:46:19 -0300

    configurando a primeira rota e a conexão com o banco de dados

---
+++ src/main/java/com/mrcs/ecommerce/Usuario/UsuarioModel.java
@@ -0,0 +1,15 @@
+package com.mrcs.ecommerce.Usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UsuarioModel {
+}
